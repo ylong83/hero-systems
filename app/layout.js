2b4289b720d5794a276c3bbd8a5f664adb63eb68
ylong83 <yvette_long@hotmail.com>
2026-03-05 14:47:35 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: "HERO Systems",
+  description: "Home Economics Resource Organiser"
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body style={{fontFamily: "Arial", padding: "40px"}}>
+        {children}
+      </body>
+    </html>
+  )
+}
