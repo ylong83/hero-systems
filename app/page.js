19f4476f1b87182bc1f826c7efcc9a381fc267fa
ylong83 <yvette_long@hotmail.com>
2026-03-05 14:38:44 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: "HERO Systems",
+  description: "Home Economics Resource Organiser"
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body style={{fontFamily:"Arial", margin:0, background:"#f4f6f8"}}>
+        {children}
+      </body>
+    </html>
+  );
+}
